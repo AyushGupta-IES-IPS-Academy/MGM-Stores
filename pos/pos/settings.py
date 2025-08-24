f2a82baff6a1b42e92569bc6db1b70f89eb88554
ayushg1806 <agupta9617@gmail.com>
2025-08-24 13:15:26 +0530

    Fix settings for Render deployment

--- pos/pos/settings.py
+++ pos/pos/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 AUTH_USER_MODEL = 'store.CustomUser'
 
