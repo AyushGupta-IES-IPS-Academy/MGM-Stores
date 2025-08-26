a33237a1d7249a6b8549c72ac1ee5fae860ef077
ayushg1806 <agupta9617@gmail.com>
2025-08-26 14:46:04 +0530

    Updated storage_backends.py

--- pos/pos/storage_backends.py
+++ pos/pos/storage_backends.py
@@ -1,11 +1,8 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
-
 class PublicSupabaseStorage(S3Boto3Storage):
-    """
-    Custom storage that forces public Supabase bucket URLs
-    instead of signed URLs.
-    """
-    def url(self, name):
-        return f"{settings.MEDIA_URL}{name}"
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    custom_domain = f"https://{settings.SUPABASE_PROJECT_REF}.supabase.co/storage/v1/object/public/{settings.AWS_STORAGE_BUCKET_NAME}"
+    default_acl = "public-read"
+    querystring_auth = False  # important for public buckets
